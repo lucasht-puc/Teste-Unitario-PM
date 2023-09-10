5722caf0ea532cfe766fafbf2d720d2880a4be69
lucasht02 <lucashemetrio@gmail.com>
2023-09-10 11:41:22 -0300

    teste livro erro

--- teste-unitario/src/src/test/java/LivroTest.java
+++ teste-unitario/src/src/test/java/LivroTest.java
@@ -1,12 +1,33 @@
 package src.test.java;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 
 import src.main.java.tdd.Livro;
 
 public class LivroTest {
+
     @Test
     public void testCriarLivro() {
-        Livro livro = new Livro();
+        Livro livro = new Livro(0, "Hobbit", "Tolkien");
+
+        assertEquals("Hobbit", livro.getTitulo());
+        assertEquals("Tolkien", livro.getAutor());
+    }
+
+    @Test
+    public void testEmprestarLivro() {
+        Livro livro = new Livro(0, "Hobbit", "Tolkien");
+        livro.emprestarLivro();
+        assertEquals(true, livro.isEmprestado());
+    } 
+
+    @Test
+    public void testRetornarLivro() {
+        Livro livro = new Livro(0, "Hobbit", "Tolkien");
+        livro.emprestarLivro();
+        livro.retornarLivro();
+        assertEquals(false, livro.isEmprestado());
     }
 }
