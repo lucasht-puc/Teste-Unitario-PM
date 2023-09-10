ba5d770a9a7044ae49d23fc6b9b75aff14edf8a6
lucasht02 <lucashemetrio@gmail.com>
2023-09-10 11:44:01 -0300

    Implementar metodos Livro

--- teste-unitario/src/src/main/java/tdd/Livro.java
+++ teste-unitario/src/src/main/java/tdd/Livro.java
@@ -1,8 +1,52 @@
 package src.main.java.tdd;
 
 public class Livro {
+    
     private int id;
     private String titulo;
     private String autor;
     private boolean emprestado;
+
+    public Livro(int id, String titulo, String autor) {
+        this.id = id;
+        this.titulo = titulo;
+        this.autor = autor;
+        this.emprestado = false;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public boolean isEmprestado() {
+        return emprestado;
+    }
+
+    public void emprestarLivro() {
+        this.emprestado = true;
+    }
+
+    public void retornarLivro() {
+        this.emprestado = false;
+    }
 }
